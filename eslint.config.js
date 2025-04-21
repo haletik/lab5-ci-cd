2e65b4d0b4423cb030422ba1f5be381a416a6167
anastasiia sineok <anastasiia.synieok@oa.edu.ua>
2025-04-21 15:49:45 +0300

    4

--- eslint.config.js
+++ eslint.config.js
@@ -4,38 +4,31 @@ import reactPlugin from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import { defineConfig } from "@eslint/config-helpers";
 
-export default defineConfig({
-  // ⛔ Ігноруємо конкретний файл
-  // ignores: ["src/main.tsx"],
-
-  // ✅ Масив конфігів, які застосовуються
-  configs: [
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    {
-      files: ["**/*.ts", "**/*.tsx"],
-      plugins: {
-        react: reactPlugin,
-        "react-hooks": reactHooks,
-      },
-      languageOptions: {
-        parser: tseslint.parser,
-        parserOptions: {
-          project: "./tsconfig.app.json",
-        },
-      },
-      settings: {
-        react: {
-          version: "detect",
-        },
-      },
-      rules: {
-        // 🔧 Основні правила
-        "react/react-in-jsx-scope": "off",
-        "no-console": "warn",
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": "warn",
+export default defineConfig([
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooks,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.app.json",
       },
     },
-  ],
-});
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "no-console": "warn",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+]);
