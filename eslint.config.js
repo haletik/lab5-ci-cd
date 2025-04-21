d6df0c08d4af362e0647394c0d7f0bc7f9bad959
haletik <92076714+haletik@users.noreply.github.com>
2025-04-21 13:11:39 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,36 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import { defineConfig } from "eslint/config";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import pluginReact from "eslint-plugin-react";
+import globals from "globals";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+
+export default defineConfig({
+ files: ["**/*.{ts,tsx,js,jsx}"],
+ languageOptions: {
+   parser: tsParser,
+   parserOptions: {
+     ecmaVersion: "latest",
+     sourceType: "module",
+   },
+   globals: {
+     ...globals.browser,
+     ...globals.node,
+   },
+ },
+ plugins: {
+   "@typescript-eslint": tseslint,
+   react: pluginReact,
+ },
+ settings: {
+   react: {
+     version: "detect",
+   },
+ },
+ rules: {
+   "react/react-in-jsx-scope": "off",
+   "no-console": "warn",
+   "no-unused-vars": "off",
+   "@typescript-eslint/no-unused-vars": "warn",
+ }
+});
